f42cd6991572365a76cbddcf1f95c84f519270ed
v.karakai <v.karakai@pin-up.tech>
2024-10-19 12:38:27 +0300

    task1 - implemented binarySearchTree

---
+++ binarySearchTree.py
@@ -0,0 +1,25 @@
+from node import Node
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+        else:
+            self._insert_recursive(self.root, new_node)
+
+    def _insert_recursive(self, current, new_node):
+        if new_node.value < current.value:
+            if current.left is None:
+                current.left = new_node
+            else:
+                self._insert_recursive(current.left, new_node)
+        else:
+            if current.right is None:
+                current.right = new_node
+            else:
+                self._insert_recursive(current.right, new_node)
